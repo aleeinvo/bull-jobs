554a44f725990d7461abf90da39feddbb9e51e41
Alee Dhillon <ali.asghar@invozone.com>
2022-05-26 19:06:04 +0500

    add fake emails

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,32 @@ var router = express.Router();
 const mailQueue = require('../services/mailQueue');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Bull Jobs' });
 });
 
 router.get('/dev', async (req, res) => {
   const tos = [
-    'aleedhx@gmail.com',
-    'ali.asghar@invozone.com',
-    'aleedhillon@gmail.com'
+    "keely.mann@hodkiewicz.com",
+    "nhamill@yahoo.com",
+    "cole.rodrigo@lehner.org",
+    "leonard92@cruickshank.com",
+    "murray.kayleigh@harris.com",
+    "tillman.meda@hotmail.com",
+    "harris.charley@gibson.com",
+    "sterling26@greenfelder.info",
+    "kay20@beer.info",
+    "gcorwin@yahoo.com",
+    "rickie16@larson.com",
+    "zieme.damien@schultz.org",
+    "xabernathy@bednar.net",
+    "lakin.ila@gmail.com",
+    "milan77@gmail.com",
+    "dagmar.trantow@dickinson.com",
+    "rosenbaum.juanita@goyette.com",
+    "jennifer.klocko@gmail.com",
+    "eric.wehner@klein.com",
+    "kayla.swaniawski@jacobi.org"
   ];
 
   mailQueue.add(tos);
